be3ed4914ace5cb484cd38b44723a6522a3517a0
BoyGK <1215356195@qq.com>
2018-08-21 10:19:18 +0800

    天气demo后台

--- src/com/bgq/demo/action/MainAction.java
+++ src/com/bgq/demo/action/MainAction.java
@@ -1,6 +1,7 @@
 package com.bgq.demo.action;
 
 import com.bgq.demo.util.HttpRequest;
+import com.bgq.demo.util.URLParams;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,15 @@ public class MainAction {
     @RequestMapping(value = "/url", method = RequestMethod.POST)
 
     public String hello(@RequestBody String param, HttpServletResponse resp) throws Exception {
-        String url = param.substring(8, param.length() - 2);
+        String str = param.substring(8, param.length() - 2);
+//        System.out.println("1" + str);
+        String[] strs = str.split("\\?");
+//        System.out.println("2-1" + strs[0]);
+//        System.out.println("2-2" + strs[1]);
+        URLParams params = new URLParams(strs[0]);
+//        System.out.println("3" + params.build());
+        String url = params.build() + strs[1];
+//        System.out.println("4" + url);
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter printWriter = resp.getWriter();
